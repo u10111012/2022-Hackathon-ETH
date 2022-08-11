2d6ddfb35c35834e9282e1d89023b8c9f1a70b94
alex <yinchihkai0909@gmail.com>
2022-08-11 16:48:59 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import './App.css';
-import Home from './pages/Home'
+import Home from './pages/Home';
 
 import {
     BrowserRouter,
@@ -10,6 +9,9 @@ import {
 
 
 function App() {
+
+    console.log('first commit')
+
     return (
         <BrowserRouter>
             <Routes>
