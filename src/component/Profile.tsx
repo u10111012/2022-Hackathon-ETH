02e6f844978ba0dce3469c51462972360ec89bea
linzhiyu <leo@rismit.io>
2022-08-11 15:27:39 +0800

    install enviroment

---
+++ src/component/Profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {useAccount, useConnect, useDisconnect} from 'wagmi'
+import {InjectedConnector} from 'wagmi/connectors/injected'
+
+
+const Profile = () => {
+
+    const {address, isConnected} = useAccount()
+    const {connect} = useConnect({
+        connector: new InjectedConnector(),
+    })
+    const {disconnect} = useDisconnect()
+    if (isConnected)
+        return (
+            <div>
+                Connected to {address}
+                <button onClick={() => disconnect()}>Disconnect</button>
+            </div>
+        )
+    return <button onClick={() => connect()}>Connect Wallet</button>
+};
+
+export default Profile;
